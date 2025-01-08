1bc7922f862b062d9b16454a8ba964e7300cd5c6
Giovan Norick Yang <giovannoricky@gmail.com>
2025-01-08 23:32:52 +0700

    refactor card layout for better responsiveness and alignment in views

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -131,13 +131,15 @@
         <div class="row row-cols-1 row-cols-md-3 row-cols-lg-3 py-3 g-3">
             @foreach ($book as $b)
                 <div class="col">
-                    <div class="card border rounded border-2 border-black">
+                    <div class="card border rounded border-2 border-black h-100">
                         <img class="card-img-top img-fluid" src="{{ asset('/storage/image/BookCover/' . $b->image) }}"
                             alt="Error" style="max-height: 500px; max-width: 100%;">
-                        <div class="card-body bg-light">
+                        <div class="card-body bg-light d-flex flex-column">
                             <h5 class="card-title text-dark fw-normal">{{ $b->title }}</h5>
                             <p class="card-subtitle text-warning mt-2 fw-bold">{{ $b->author }}</p>
-                            <a class="btn btn-outline-dark mt-2" href="/bookDetail/{{ $b->id }}">Detail</a>
+                            <div class="mt-auto">
+                                <a class="btn btn-outline-dark mt-2" href="/bookDetail/{{ $b->id }}">Detail</a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -9,13 +9,16 @@
         <div class="row row-cols-2 row-cols-md-2 row-cols-lg-4 py-3 g-3">
             @foreach ($publisher as $p)
                 <div class="col">
-                    <div class="card border rounded border-2 border-black">
+                    <div class="card border rounded border-2 border-black h-100">
                         <img height="250px" width="250px" class="card-img-top"
                             src="{{ asset('/storage/image/PublisherLogo/' . $p->image) }}" alt="Error">
-                        <div class="card-body bg-light">
-                            <h5 class="card-title text-dark fw-normal">{{ $p->name }}</h5>
-                            <p class="card-subtitle text-warning mt-2 fw-bold">{{ $p->address }}</p>
-                            <a class="btn btn-outline-dark mt-2" href="/publisherDetail/{{ $p->id }}">Detail</a>
+                        <div class="card-body bg-light d-flex flex-column">
+                            <div class="mt-auto">
+                                <h5 class="card-title text-dark fw-normal">{{ $p->name }}</h5>
+                                <p class="card-subtitle text-warning mt-2 fw-bold">{{ $p->address }}</p>
+
+                                <a class="btn btn-outline-dark mt-2" href="/publisherDetail/{{ $p->id }}">Detail</a>
+                            </div>
                         </div>
                     </div>
                 </div>
